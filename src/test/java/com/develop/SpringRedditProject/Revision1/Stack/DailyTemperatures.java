f287b5dd33e216f7546fd828bd651dcd87898717
himanshurajStayBloom <himanshu.raj@staybloom.com>
2023-12-01 08:12:39 +0530

    added new question

--- src/test/java/com/develop/SpringRedditProject/Revision1/Stack/DailyTemperatures.java
+++ src/test/java/com/develop/SpringRedditProject/Revision1/Stack/DailyTemperatures.java
@@ -1,4 +1,34 @@
 package com.develop.SpringRedditProject.Revision1.Stack;
+import java.util.Arrays;
+import java.util.Stack;
+public class DailyTemperatures {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
 
-public class dailyTemperatures {
+        int[] res = new int[temperatures.length];
+        res[temperatures.length - 1] = 0;
+        stack.push(temperatures.length - 1);
+
+        for(int i = temperatures.length - 2; i >= 0 ; i--) {
+            while(stack.size() > 0 && temperatures[i] > temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if(stack.size() == 0) {
+                res[i] = 0;
+                stack.push(i);
+            } else {
+                res[i] = stack.peek() - i;
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {89,62,70,58,47,47,46,76,100,70};
+        int res[] = dailyTemperatures(ar);
+        for(int i = 0 ; i < ar.length ; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
 }
--- src/test/java/com/develop/SpringRedditProject/Revision1/Stack/MyQueue.java
+++ src/test/java/com/develop/SpringRedditProject/Revision1/Stack/MyQueue.java
@@ -1,4 +1,48 @@
 package com.develop.SpringRedditProject.Revision1.Stack;
 
-public class ImplQueue {
+import java.util.Stack;
+
+public class MyQueue {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+        while(stack1.size() > 0) {
+            stack2.push(stack1.pop());
+        }
+        stack1.push(x);
+        while (stack2.size() > 0) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    public int pop() {
+        return stack1.pop();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean empty() {
+        if(stack1.size() == 0)
+            return false;
+        else {
+            return true;
+        }
+    }
+
+    public static void main(String[]args){
+
+        MyQueue obj = new MyQueue();
+    //    obj.push(x);
+        int param_2 = obj.pop();
+        int param_3 = obj.peek();
+        boolean param_4 = obj.empty();
+    }
 }
+
+
